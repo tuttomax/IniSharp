3669d595b8669c142843409e79f284ad760e94d1
tuttomax <tuttomax@users.noreply.github.com>
2014-08-30 15:18:05 +0200

    Create ParsingException.cs

---
+++ Exceptions/ParsingException.cs
@@ -0,0 +1,14 @@
+namespace IniSharp.Exceptions
+{
+    internal class ParsingException : Exception
+    {
+        public ParsingException()
+        {
+        }
+
+        public ParsingException(string message)
+            : base(message)
+        {
+        }
+    }
+}
